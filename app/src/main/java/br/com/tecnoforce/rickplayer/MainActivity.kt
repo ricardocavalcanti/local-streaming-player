80b7a3cd5c07fc76adee6d34608865c401fc7617
Ricardo Cavalcanti <ricardocavalcanti@outlook.com>
2025-04-18 18:08:32 -0300

    Created remote control navigation

--- app/src/main/java/br/com/tecnoforce/rickplayer/MainActivity.kt
+++ app/src/main/java/br/com/tecnoforce/rickplayer/MainActivity.kt
@@ -3,6 +3,7 @@ package br.com.tecnoforce.rickplayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -17,7 +18,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import br.com.tecnoforce.rickplayer.ui.theme.RickPlayerTheme
@@ -67,16 +74,26 @@ fun VideoListScreen() {
 // Item individual da lista
 @Composable
 fun VideoListItem(name: String) {
+    var isFocused by remember { mutableStateOf(false) }
+
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 4.dp),
+            .padding(vertical = 4.dp)
+            .onFocusChanged {
+                isFocused = it.isFocused
+            }
+            .focusable(),
+        colors = CardDefaults.cardColors(
+            containerColor = if (isFocused) Color(0xFF4444AA) else Color.White
+        ),
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
     ) {
         Text(
             text = name,
             modifier = Modifier.padding(16.dp),
-            style = MaterialTheme.typography.bodyLarge
+            style = MaterialTheme.typography.bodyLarge,
+            color = if (isFocused) Color.White else Color.Black
         )
     }
 }
